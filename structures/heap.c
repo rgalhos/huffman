6614faf51c54da410728e3fbfafeef214e1e153c
rgalhos <44909799+rgalhos@users.noreply.github.com>
2019-08-13 13:12:37 -0300

    Added function pointer in heap struct

--- structures/heap.c
+++ structures/heap.c
@@ -6,6 +6,7 @@
 #endif
 
 typedef struct heap_t {
+	int (*getter)(struct heap_t *, int);
 	int maxSize;
 	int size;
 	void **data;
@@ -15,19 +16,41 @@ int getParentIndex(heap_t *heap, int index) { return index >> 1; }
 int getLeftIndex(heap_t *heap, int index) { return index << 1; }
 int getRightIndex(heap_t *heap, int index) { return (index << 1) + 1; }
 int isHeapEmpty(heap_t *heap) { return !heap->size; }
-int getSize(heap_t *heap) { return heap->size; }
+int getHeapSize(heap_t *heap) { return heap->size; }
 void* peek(heap_t *heap, int index) { return heap->data[index]; }
 
-heap_t* createHeap(int maxSize) {
+heap_t* createHeap(int maxSize, int (*getter)(heap_t *, int)) {
 	heap_t *newHeap = (heap_t *)malloc(sizeof(heap_t));
 	newHeap->data = (void **)malloc(maxSize * sizeof(void *));
 	newHeap->maxSize = maxSize;
+	newHeap->getter = getter;
 	newHeap->size = 0;
 
 	return newHeap;
 }
 
-void enqueue(heap_t *heap, void *item, int (*getElement)(heap_t *, int)) {
+void minHeapify(heap_t *heap, int i) {
+	int leftIndex = getLeftIndex(heap, i);
+	int rightIndex = getRightIndex(heap, i);
+	int largest;
+
+	if (leftIndex <= heap->size && heap->getter(heap, leftIndex) < heap->getter(heap, i)) {
+		largest = leftIndex;
+	} else {
+		largest = i;
+	}
+
+	if (rightIndex <= heap->size && heap->getter(heap, rightIndex) < heap->getter(heap, largest)) {
+		largest = rightIndex;
+	}
+
+	if (heap->data[i] != heap->data[largest]) {
+		swap(heap->data[i], heap->data[largest], void *);
+		minHeapify(heap, largest);
+	}
+}
+
+void enqueue(heap_t *heap, void *item) {
 	if (heap->size >= heap->maxSize) {
 		fprintf(stderr, "Heap overflow (%d items)\n", heap->size);
 		return;
@@ -40,7 +63,7 @@ void enqueue(heap_t *heap, void *item, int (*getElement)(heap_t *, int)) {
 
 	// e(key) < e(index) ---> minHeapify
 	// e(key) > e(index) ---> maxHeapify
-	while (parentIndex >= 1 && getElement(heap, keyIndex) < getElement(heap, parentIndex)) {
+	while (parentIndex >= 1 && heap->getter(heap, keyIndex) < heap->getter(heap, parentIndex)) {
 		swap(heap->data[keyIndex], heap->data[parentIndex], void *);
 
 		keyIndex = parentIndex;
@@ -56,30 +79,9 @@ void* dequeue(heap_t *heap) {
 
 	void *item = heap->data[1];
 	heap->data[1] = heap->data[heap->size--];
-	//maxHeapify(heap, 1);
+	minHeapify(heap, 1);
 
 	return item;
-}
-
-void minHeapify(heap_t *heap, int i, int (*getElement)(heap_t *, int)) {
-	int leftIndex = getLeftIndex(heap, i);
-	int rightIndex = getRightIndex(heap, i);
-	int largest;
-
-	if (leftIndex <= heap->size && getElement(heap, leftIndex) < getElement(heap, i)) {
-		largest = leftIndex;
-	} else {
-		largest = i;
-	}
-
-	if (rightIndex <= heap->size && getElement(heap, rightIndex) < getElement(heap, largest)) {
-		largest = rightIndex;
-	}
-
-	if (heap->data[i] != heap->data[largest]) {
-		swap(heap->data[i], heap->data[largest], void *);
-		minHeapify(heap, largest, getElement);
-	}
 }
 
 #endif
